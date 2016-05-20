9afc789fb44eda79be9397771774341e755d8efb
arpan-jain <arpanjain3.14@gmail.com>
2016-05-20 16:35:55 +0530

    Flux Changes

--- src/js/components/Article.js
+++ src/js/components/Article.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export default class extends React.Component {
   render() {
-    const { title } = this.props;
-
+     var title  = this.props.title;
+      console.log(this.props.title);
     return (
       <div class="col-md-4">
-        <h4>{title}</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
+        <h4>Title:  {title}</h4>
+        <p>This is the admin Dashboard. Please wait while all other content is being developed.</p>
         <a class="btn btn-default" href="#">More Info</a>
       </div>
     );
